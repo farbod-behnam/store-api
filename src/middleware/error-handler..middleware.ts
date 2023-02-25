ab0b68bdeee64606e852eaf6b734f4cd794cfa96
farbod-behnam <farbod.behnam@gmail.com>
2023-02-25 10:53:32 +0330

    change Logger class name

--- src/middleware/error-handler..middleware.ts
+++ src/middleware/error-handler..middleware.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express"
-import Logging from "../library/Logging"
+import Logger from "../library/Logging"
 
-export default async function errorHandlerMiddleware (err: Error, req: Request, res: Response, next: NextFunction)  {
-  Logging.log(err);
-  return res.status(500).json({ msg: 'Something went wrong, please try again' })
+export default async function errorHandlerMiddleware(err: Error, req: Request, res: Response, next: NextFunction) {
+  Logger.error(err);
+  return res.status(500).json({ msg: err });
 }
+
 
--- src/middleware/request-logger.middleware.ts
+++ src/middleware/request-logger.middleware.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import Logging from "../library/Logging";
+import Logger from "../library/Logging";
 
 export default function requestLogger(req: Request, res: Response, next: NextFunction) {
-    Logging.info(req.method + " " + req.url);
+    Logger.info(req.method + " " + req.url);
     next();
 }
